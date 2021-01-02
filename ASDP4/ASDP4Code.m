5f7ae25cbae7efa8c801d6d75b289a9ebacef54c
2306781M <74727598+2306781M@users.noreply.github.com>
2021-01-02 20:05:06 +0000

    Add files via upload

---
+++ ASDP4/ASDP4Code.m
@@ -0,0 +1,7 @@
+%Define inputs to be run before coding
+
+%Define any constants to be used
+
+FlightDataOutput={'test','test','test','test','test','test','test'}; %make this actually use the right values
+
+open_system(test)
